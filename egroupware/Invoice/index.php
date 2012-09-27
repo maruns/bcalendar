b999d115557c497b8750c7c538baab029aae214c
root <root@new-host.home>
2012-09-28 00:50:01 +0200

    Poprawki z "Downloads"

--- egroupware/Invoice/index.php
+++ egroupware/Invoice/index.php
@@ -5,8 +5,8 @@ $GLOBALS['egw_info'] = array(
                 'noheader'   => True
         ),
 );
-require_once('../SmartyConfig.php');
 require_once('../DatabaseConnection.php');
+require_once('../SmartyConfig.php');
 include_once('/var/lib/egroupware/Invoice.php');
 $result = SendQuery("select `egw_addressbook`.`n_fn`, `egw_addressbook`.`account_id` from `egw_addressbook` inner join `egw_accounts` on `egw_addressbook`.`account_id` = `egw_accounts`.`account_id` where `egw_accounts`.`account_type` = 'u' and `egw_accounts`.`account_primary_group` = -329 order by `egw_addressbook`.`n_family`, `egw_addressbook`.`n_given`, `egw_addressbook`.`n_middle`");
 while ($row = GetNextRow($result))
